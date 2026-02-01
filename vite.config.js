29fd582fb146ca77fa5d44f7c52d260e55704a3a
Hugh McKenna <hfmckenna@outlook.com>
2026-02-01 13:53:08 +0000

    feat: Subfolder for blog

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import path from 'path';
 
 export default {
+    root: path.resolve(__dirname, 'src'),
+    publicDir: path.resolve(__dirname, 'src'),
     build: {
         outDir: path.resolve(__dirname, 'dist'),
         emptyOutDir: true,
